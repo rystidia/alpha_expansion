88266d69d3a95afc5fcc988834ef702b16043c6f
rystidia <rystidia@users.noreply.github.com>
2026-04-22 12:20:11 +0000

    deploy: 221a94283e7b8cefc4215e980cda379bdedb6b92

--- search/functions_9.js
+++ search/functions_9.js
@@ -4,8 +4,9 @@ var searchData=
   ['set_5fedge_5fweights_1',['set_edge_weights',['../classEnergyModel.html#a4caeb05b405f72a1117e26be15846c7d',1,'EnergyModel']]],
   ['set_5flabel_2',['set_label',['../classEnergyModel.html#a266792d3153ec163896849bbafefa33e',1,'EnergyModel']]],
   ['set_5flabels_3',['set_labels',['../classEnergyModel.html#a8a38ad1256537c9c82e39c84d778b8c8',1,'EnergyModel']]],
-  ['set_5fpairwise_5fcost_5ffn_4',['set_pairwise_cost_fn',['../classEnergyModel.html#a6a2703b6c72c216f2102eac925d6b12b',1,'EnergyModel']]],
-  ['set_5fpairwise_5fcosts_5',['set_pairwise_costs',['../classEnergyModel.html#aed95bf5533c6bd7eb10117501beaf4c3',1,'EnergyModel']]],
-  ['set_5funary_5fcost_5ffn_6',['set_unary_cost_fn',['../classEnergyModel.html#a2c111a6bc1e4dd411467c3fc9f6a9202',1,'EnergyModel']]],
-  ['set_5funary_5fcosts_7',['set_unary_costs',['../classEnergyModel.html#a663637a2236ba8695cc7174826c4a5a7',1,'EnergyModel']]]
+  ['set_5fon_5fiteration_4',['set_on_iteration',['../classAlphaExpansion.html#a68e3b778cb3fdb3766a262f33d139f37',1,'AlphaExpansion']]],
+  ['set_5fpairwise_5fcost_5ffn_5',['set_pairwise_cost_fn',['../classEnergyModel.html#a6a2703b6c72c216f2102eac925d6b12b',1,'EnergyModel']]],
+  ['set_5fpairwise_5fcosts_6',['set_pairwise_costs',['../classEnergyModel.html#aed95bf5533c6bd7eb10117501beaf4c3',1,'EnergyModel']]],
+  ['set_5funary_5fcost_5ffn_7',['set_unary_cost_fn',['../classEnergyModel.html#a2c111a6bc1e4dd411467c3fc9f6a9202',1,'EnergyModel']]],
+  ['set_5funary_5fcosts_8',['set_unary_costs',['../classEnergyModel.html#a663637a2236ba8695cc7174826c4a5a7',1,'EnergyModel']]]
 ];
